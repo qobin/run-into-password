9981e553ea7253dc2b21be7d64751edb7721f6a2
qobin <qobin.quan@shein.com>
2019-06-15 18:40:49 +0800

    update

--- password_tools/create_password.py
+++ password_tools/create_password.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+
 tup_num = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 tup_words = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
               'u', 'v', 'w', 'x', 'y', 'z')
@@ -10,12 +11,51 @@ top_spec = ('!', '@', '#', '$', '%')
 
 tup_total = tup_num + tup_words + tup_big + top_spec
 
-list_password = []
-def createPassword( num ):
-    if num < 4 or num >= 12:
-        return 0
-    else:
-        for i in (0, len(tup_total)):
-            str1 = ""
-            for j in range(0, num):
-                str1 += tup_total[i]
+
+# 承载生成的密码
+# password： 明文密码
+# password_md5：md5加密密码
+# password_sha：sha家秘密吗
+class PasswordObj:
+
+    def __init__(self, password, password_md5, password_sha):
+        self.password = password
+        self.password_md5 = password_md5
+        self.password_sha = password_sha
+
+    def __str__(self):
+        return "password: " + self.password + ",password_md5: " + self.password_md5 \
+               + ",password_sha: " + self.password_sha
+
+
+from password_tools.password_clock import Password
+import hashlib
+
+
+def create_password(num, head=None):
+    list_password = []
+    i = 0
+    if 4 <= num < 12:
+        password_creator = Password(tup_total, head="1", num=num)
+        md5_tools = hashlib.md5()
+        sha256_tools = hashlib.sha256()
+        while password_creator.has_next():
+            password_str = password_creator.next_order_password()
+            md5_tools.update(password_str)
+            sha256_tools.update(password_str)
+            obj = PasswordObj(password_str, md5_tools.hexdigest(), sha256_tools.hexdigest())
+            list_password.append(obj)
+        return list_password
+            # if i < 10:
+            #     list_password.append(obj)
+            # else:
+            #     i = 0
+            #     list_password.clear()
+            #     list_password.append(obj)
+            # print list_password
+
+
+if __name__ == "__main__":
+    list_total = create_password(num=4)
+    for obj in list_total:
+        print obj
--- password_tools/password_clock.py
+++ password_tools/password_clock.py
@@ -2,58 +2,94 @@
 # -*- coding:utf-8 -*-
 
 import logging
-import create_password
 
 
-class Password():
+# 密码生成器
+class Password:
 
-    def __init__(self, hashTup, currentWords=None, num=4, head="0"):
-        self.hashTup = hashTup
-        self.totalSize = len(hashTup)
-        if not self.totalSize:
-            self.totalSize = 0
+    def __init__(self, hash_tup, current_words=None, num=4, head=None):
+        self.hash_tup = hash_tup
+        self.total_size = len(hash_tup)
+        if not self.total_size:
+            self.total_size = 0
         self.num = num
         self.head = head
-        if not currentWords:
-            self.currentWords = ""
-            for i in range(0, num):
-                self.currentWords += head
+        if not current_words:
+            self.current_words = ""
+            if not head:
+                head = "0"
+            self.current_words += head
+            for i in range(1, num):
+                self.current_words += self.hash_tup[0]
         else:
-            self.currentWords = currentWords
+            self.current_words = current_words
 
-    def getNextPassword(self, count=1):
+    # 顺序密码生成器
+    # count 为间隔
+    # 设置不同的首字母可以用于分片
+    # 如果不设置首字母则进行全局唯一
+    def next_order_password(self, count=1):
         """
         use in get net password
         default add 1
         :return:
         """
-        if self.totalSize <= 0:
+        if self.total_size <= 0:
             logging.warning("hash map is null")
-            return self.currentWords
         else:
-            return self.__add()
+            current_list = list(self.current_words)
+            if self.head:
+                current_list.remove(current_list[0])
+                self.current_words = self.head + self.__add(current_list=current_list, count=count)
+            else:
+                self.current_words = self.__add(current_list=list(self.current_words), count=count)
+        return self.current_words
 
-    def __add(self, currentList=None, pos=-1, count=1):
+    # 顺序密码生成器算法规则
+    # 思想四不确定进制
+    # 类似于10进制，24进制，60进制等等
+    def __add(self, current_list=None, pos=-1, count=1):
         """
         password add a count
         default last element
         :param count:
         :return:
         """
-        if not currentList:
-            currentList = list(self.currentWords)
-        currentPos = self.hashTup.index(currentList[pos]) + count
-        if currentPos >= self.totalSize:
-            currentList[pos] = self.hashTup[0]
-            self.__add(currentList, pos - 1, 1)
+        if not current_list:
+            current_list = list(self.current_words)
+        currentPos = self.hash_tup.index(current_list[pos]) + count
+        if currentPos >= self.total_size:
+            current_list[pos] = self.hash_tup[0]
+            self.__add(current_list, pos - 1, 1)
         else:
-            currentList[pos] = self.hashTup[currentPos]
-        self.currentWords = "".join(currentList)
-        return self.currentWords
+            current_list[pos] = self.hash_tup[currentPos]
+        return "".join(current_list)
 
+    # 返回是否还有下一个密码
+    def has_next(self):
+        """
+        has_next_password?
+        yes result true
+        no result false
+        :return:
+        """
+        current_list = list(self.current_words)
+        last_word = self.hash_tup[-1]
+        if self.head:
+            current_list.remove(current_list[0])
+        for w in current_list:
+            if w != last_word:
+                return True
+        return False
+
+
+import create_password
 
 if __name__ == "__main__":
-    password = Password(create_password.tup_total )
-    for i in range(0, 10000):
-        passstr = password.getNextPassword()
-        print passstr
+    password = Password(create_password.tup_total, head="a", num=6)
+    while True:
+        strPassword = password.next_order_password()
+        print strPassword
+        if not password.has_next():
+            print "create password end"
+            break
